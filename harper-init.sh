e9af0af39a9a6e60b37d0f14d26a5cb02a004100
Paolo Pragides <paolo.pragides@sjrb.ca>
2018-02-10 14:21:21 -0700

    Added curl commands for setting up schema, table and loading dummy data into harperdb

--- harper-init.sh
+++ harper-init.sh
@@ -1,8 +1,34 @@
 #!/bin/bash
 
-## Quick shell script that initializes our HarperDB schema
-HARPER_PROTOCOL: "http"
-HARPER_HOST="localhost"
-HARPER_PORT="9925"
+## Quick shell script that initializes our HarperDB schema, table and dummy data
 
-## Also used to populate some dummy data into HarperDB for demo purposes
+## Create schema
+curl -v --url http://localhost:9925 \
+    --header 'Authorization: Basic' \
+    --header 'Content-Type: application/json' \
+    --data '{
+        "operation": "create_schema",
+        "schema": "golftv_dev"
+    }'
+
+## Create table
+curl -v --url http://localhost:9925 \
+    --header 'Authorization: Basic' \
+    --header 'Content-Type: application/json' \
+    --data '{
+        "operation": "create_table",
+        "schema": "golftv_dev",
+        "table": "golftv_dev",
+        "hash_attribute": "id"
+    }'
+
+## Import demo_data.csv to populate data
+curl -v --url http://localhost:9925 \
+    --header 'Authorization: Basic' \
+    --header 'Content-Type: application/json' \
+    --data '{
+        "operation": "csv_file_load",
+        "schema": "golftv_dev",
+        "table": "golftv_dev",
+        "file_path": "demo_data.csv"
+    }'
